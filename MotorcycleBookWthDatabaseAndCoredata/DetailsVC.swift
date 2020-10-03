db0329b431063e39cb2f18d028a3b8ab69b14935
Ali Ertekin Elkoca <30657493+ertekinelkoca@users.noreply.github.com>
2020-10-04 00:26:35 +0300

    imagepicker and coredata

--- MotorcycleBookWthDatabaseAndCoredata/DetailsVC.swift
+++ MotorcycleBookWthDatabaseAndCoredata/DetailsVC.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import CoreData
 
-class DetailsVC: UIViewController {
+class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     
     @IBOutlet weak var imageView: UIImageView!
@@ -20,8 +21,36 @@ class DetailsVC: UIViewController {
 
         // Do any additional setup after loading the view.
         
+        
+        
+        
+        
+        //Recognizers
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(gestureRecognizer)
+         
+        // to make image clickable
+        imageView.isUserInteractionEnabled = true
+        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageTapRecognizer)
+    }
+    
+    
+    //to redirect user to gallery
+    @objc func selectImage(){
+        
+        //to reach users media and libraries
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
     }
 
     @objc func hideKeyboard(){
@@ -32,7 +61,32 @@ class DetailsVC: UIViewController {
     
     @IBAction func saveButtonClicked(_ sender: Any) {
         
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
         
+        
+        let newMotorcycle = NSEntityDescription.insertNewObject(forEntityName: "Motorcycle", into: context)
+        
+        
+        
+        //attributes
+        newMotorcycle.setValue(modelText.text, forKey: "model")
+        newMotorcycle.setValue(typeText.text, forKey: "type")
+        if let year = Int(yearText.text!){
+            
+            newMotorcycle.setValue(year, forKey: "year")
+        }
+        newMotorcycle.setValue(UUID(), forKey: "id")
+        
+        let data = imageView.image?.pngData()
+        newMotorcycle.setValue(data, forKey: "image")
+      
+        do{
+            try context.save()
+            print("success")
+        }catch{
+            print("error")
+        }
         
     }
     
